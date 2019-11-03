b86d8ae8936e251bc739135bd20439927e5bd725
DavidVallecios <00078217@uca.edu.sv>
2019-11-03 17:17:18 -0600

    Updates

---
+++ precache-manifest.91f9d871e01127dc84efcb6d29012900.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915047329a73acb02f82ec0f105af3b3",
+    "url": "/ReactJSL9/index.html"
+  },
+  {
+    "revision": "959f198bc6bbc5045fed",
+    "url": "/ReactJSL9/static/css/main.e8dad84f.chunk.css"
+  },
+  {
+    "revision": "a923cfbb553af830aab5",
+    "url": "/ReactJSL9/static/js/2.cb7ee25b.chunk.js"
+  },
+  {
+    "revision": "959f198bc6bbc5045fed",
+    "url": "/ReactJSL9/static/js/main.3c798829.chunk.js"
+  },
+  {
+    "revision": "3a472e3c19d7e3cf80bc",
+    "url": "/ReactJSL9/static/js/runtime-main.48a5db44.js"
+  }
+]);
